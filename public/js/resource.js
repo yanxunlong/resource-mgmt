87a7cf8fbcec18cb7e77363398979a31ecd22990
Your Name <2301721C@student.tp.edu.sg>
2024-11-14 09:33:24 +0800

    Completed the setup of testing environment

--- public/js/resource.js
+++ public/js/resource.js
@@ -34,3 +34,64 @@ function addResource() {
     };
     request.send(JSON.stringify(jsonData));
 }
+
+function editResource(data) {
+    var selectedResource = JSON.parse(data);
+    document.getElementById("editName").value = selectedResource.name;
+    document.getElementById("editLocation").value = selectedResource.location;
+    document.getElementById("editDescription").value = selectedResource.description;
+    document.getElementById("editOwner").value = selectedResource.owner;
+    document.getElementById("updateButton").setAttribute("onclick", 'updateResource("' +
+        selectedResource.id + '")');
+    $('#editResourceModal').modal('show');
+}
+function updateResource(id) {
+    console.log(id)
+    var response = "";
+    var jsonData = new Object();
+    jsonData.name = document.getElementById("editName").value;
+    jsonData.location = document.getElementById("editLocation").value;
+    jsonData.description = document.getElementById("editDescription").value;
+    jsonData.owner = document.getElementById("editOwner").value;
+    if (jsonData.name == "" || jsonData.location == "" || jsonData.description == "" ||
+        jsonData.owner == "") {
+        document.getElementById("editMessage").innerHTML = 'All fields are required!';
+        document.getElementById("editMessage").setAttribute("class", "text-danger");
+        return;
+    }
+    var request = new XMLHttpRequest();
+    request.open("PUT", "/edit-resource/" + id, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onload = function () {
+        response = JSON.parse(request.responseText);
+        if (response.message == "Resource modified successfully!") {
+            document.getElementById("editMessage").innerHTML = 'Edited Resource: ' +
+                jsonData.name + '!';
+            document.getElementById("editMessage").setAttribute("class",
+                "text-success");
+            window.location.href = 'index.html';
+        }
+        else {
+            document.getElementById("editMessage").innerHTML = 'Unable to edit resource!';
+            document.getElementById("editMessage").setAttribute("class", "text-danger");
+        }
+    };
+    request.send(JSON.stringify(jsonData));
+}
+
+function deleteResource(selectedId) {
+    var response = "";
+    var request = new XMLHttpRequest();
+    request.open("DELETE", "/delete-resource/" + selectedId, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onload = function () {
+        response = JSON.parse(request.responseText);
+        if (response.message == "Resource deleted successfully!") {
+            window.location.href = 'index.html';
+        }
+        else {
+            alert('Unable to delete resource!');
+        }
+    };
+    request.send();
+}
