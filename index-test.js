b961917eaf38e477768d8a99a425a88bdeed09ff
Your Name <2301721C@student.tp.edu.sg>
2024-11-20 15:50:36 +0800

    Completed the setup of testing environment

---
+++ index-test.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+const PORT = process.env.PORT || 5050
+var startPage = "index.html";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("./instrumented"));
+
+const { addResource, viewResources, editResource, deleteResource } = require('./utils/ResourceUtil');
+app.post('/add-resource', addResource);
+app.get('/view-resources', viewResources);
+app.put('/edit-resource/:id', editResource);
+app.delete('/delete-resource/:id', deleteResource);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/instrumented/" + startPage);
+})
+
+server = app.listen(PORT, function () {
+    const address = server.address();
+    const baseUrl = `http://${address.address == "::" ? 'localhost' :
+address.address}:${address.port}`;
+    console.log(`Demo project at: ${baseUrl}`);
+});
+
+module.exports = {app, server}
